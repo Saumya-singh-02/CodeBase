3a06f4da8c79c22b0c44bf566354d1ca162ff08d
Saumya-singh-02 <76607453+Saumya-singh-02@users.noreply.github.com>
2023-10-20 10:20:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Form a number divisible by 3 using array digits/form-a-number-divisible-by-3-using-array-digits.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int isPossible(int N, int arr[]) {
+        string sum="";
+        for(int i=0;i<N;i++){
+            sum+=to_string(arr[i]);
+        }
+        int t=0;
+        for(int i=0;i<sum.length();i++){
+            t+=sum[i]-'0';
+        }
+        return t%3==0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+
+        Solution ob;
+        cout << ob.isPossible(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
